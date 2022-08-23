7cbdcc0454a36018e2491dfdec4840ec39bc5ce1
Mia Stroud <miast36@gmail.com>
2022-08-22 22:45:40 -0400

    medium level challenge

---
+++ sumOddEven.js
@@ -0,0 +1,13 @@
+function sumOddAndEven(arr) {
+  return arr.reduce(
+    (a, c) => {
+      c % 2 === 0 ? (a[0] += c) : (a[1] += c);
+      return a;
+    },
+    [0, 0]
+  );
+}
+
+const test1 = [1, 2, 3, 4];
+const summed = sumOddAndEven(test1);
+console.log(summed);
